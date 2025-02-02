33c524101bd7309439a9e5120e995d6f3dfbed0d
lintong <60266082+alinangnaiba@users.noreply.github.com>
2025-02-02 00:05:08 +0000

    remove cred options

--- Migrator.Infrastructure/AzureServices/AzureFileStorageService.cs
+++ Migrator.Infrastructure/AzureServices/AzureFileStorageService.cs
@@ -16,24 +16,24 @@ namespace Migrator.Infrastructure.AzureServices
 
         public byte[]? GetByte(string fileName)
         {
-            var credential = new DefaultAzureCredential(new DefaultAzureCredentialOptions
-            {
-                ExcludeEnvironmentCredential = true,
-                ExcludeManagedIdentityCredential = false,
-                ExcludeVisualStudioCredential = true,
-                ExcludeSharedTokenCacheCredential = true
-            });
-            try
-            {
-                var token = credential.GetToken(new Azure.Core.TokenRequestContext(new[] { "https://storage.azure.com//.default" }));
-                Console.WriteLine($"✅ Managed Identity Authentication Succeeded! Token Expires: {token.ExpiresOn}");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"❌ Managed Identity Authentication Failed: {ex.Message}");
-            }
+            //var credential = new DefaultAzureCredential(new DefaultAzureCredentialOptions
+            //{
+            //    ExcludeEnvironmentCredential = true,
+            //    ExcludeManagedIdentityCredential = false,
+            //    ExcludeVisualStudioCredential = true,
+            //    ExcludeSharedTokenCacheCredential = true
+            //});
+            //try
+            //{
+            //    var token = credential.GetToken(new Azure.Core.TokenRequestContext(new[] { "https://storage.azure.com//.default" }));
+            //    Console.WriteLine($"✅ Managed Identity Authentication Succeeded! Token Expires: {token.ExpiresOn}");
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine($"❌ Managed Identity Authentication Failed: {ex.Message}");
+            //}
 
-            ShareClient share = new ShareClient(new Uri($"{_settings.Uri}/{_settings.ShareName}"), credential);
+            ShareClient share = new ShareClient(new Uri($"{_settings.Uri}/{_settings.ShareName}"), new DefaultAzureCredential());
             ShareDirectoryClient client = share.GetDirectoryClient(_settings.CertificateDirectoryName);
             var list = client.GetFilesAndDirectories().ToList();
             var fileItem = list.FirstOrDefault(file => !file.IsDirectory && file.Name == fileName);
